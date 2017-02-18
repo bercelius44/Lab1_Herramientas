ee3e63d61ebf746e784168de9d28e192fe32590e
David Palacios <bercelius-44@hotmail.com>
2017-02-17 20:03:51 -0500

    Se agrego con exito la funcion de multiplicar, se corrigieron los warnings y se corrigieron aspectos del menu de la calculadora kawaii

--- lab1.c
+++ lab1.c
@@ -2,9 +2,10 @@
 #include <string.h>
 #include <math.h>
 
-
-int sum();
-
+int sum(int num1, int num2);
+int Subtract(int num1, int num2);
+int multiply(int num1, int num2);
+void dec_bin(int n);
 
 int main()
 {
@@ -16,38 +17,34 @@ int main()
 	printf("                          2. Subtract two numbers.\n");
 	printf("                          3. Multiply two numbres.\n\n");
 	printf("           The result will be in binary.\n");
-	printf("           Please enter the numbers you want to operate:\n");
 	printf("           Which task do you want me to do? (1)(2)(3)\n");
 	printf("           ");
 	scanf("%d%*c",&i_i_menu_rta);
+	if (i_i_menu_rta!=1 && i_i_menu_rta!=2 && i_i_menu_rta!=3) {
+		printf("           Your choose it's invalid, please select other one: \n" );
+		printf("           ");
+		scanf("%d%*c",&i_i_menu_rta);
+	}
+	printf("           Please enter the numbers you want to operate:\n");
 	printf("           Number 1: ");
 	scanf("%d%*c",&i_i_num1);
 	printf("           Number 2: ");
 	scanf("%d%*c",&i_i_num2);
 
 
-	if (i_i_menu_rta!=1 && i_i_menu_rta!=2 && i_i_menu_rta!=3) {
 
-		printf("\n           Your choose it's invalid, please select other one: \n" );
-		scanf("%d%*c",&i_i_menu_rta);
-
-	}
-
-switch (i_i_menu_rta) {
-	case 1: sum_return=sum(i_i_num1,i_i_num2);
-					dec_bin(sum_return);
-
-	break;
-	case 2: sub_return=Subtract(i_i_num1,i_i_num2);
-					dec_bin(sub_return);
-	break;
-	case 3: mult_return=multiply();
-					printf("           Your number is %d\n",mult_return);
-	break;
+	switch (i_i_menu_rta) {
+		case 1: sum_return=sum(i_i_num1,i_i_num2);
+						dec_bin(sum_return);
+		break;
+		case 2: sub_return=Subtract(i_i_num1,i_i_num2);
+						dec_bin(sub_return);
+	  break;
+	  case 3: mult_return=multiply(i_i_num1,i_i_num2);
+						dec_bin(mult_return);
+	  break;
+  }
 }
-
-}
-
 
 int sum(int num1, int num2) {
 					return num1+num2;
@@ -57,19 +54,12 @@ int Subtract(int num1, int num2){
 					return num1-num2;
 }
 
-int multiply(){
-
-					int num1,num2;
-					printf("           Please enter the numbers you want to multiply: \n");
-					printf("           Number 1: ");
-					scanf("%d%*c",&num1);
-					printf("           Number 2: ");
-					scanf("%d%*c",&num2);
-					return num1/num2;
-
+int multiply(int num1, int num2){
+					return num1*num2;
 }
 
 void dec_bin(int n){
+
 	long long binaryNumber = 0;
 	int remainder, i = 1, step = 1;
 
@@ -80,6 +70,5 @@ void dec_bin(int n){
 			binaryNumber += remainder*i;
 			i *= 10;
 	}
-	  printf("           Your answer is: %ld\n", binaryNumber);
-	return 0;
+	  printf("           Your answer is: %lld\n", binaryNumber);
 }
